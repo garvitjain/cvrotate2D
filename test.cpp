d42b61248238d802ae4e651f8e57b953069ee728
Manuel Ignacio López Quintero <milqmail@gmail.com>
2014-03-06 16:39:17 +0100

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+// Compìle with: g++ test.cpp cvrotate2D.cpp -lopencv_core -lopencv_highgui -lopencv_imgproc
+
+#include "cvRotate2D.hpp"
+
+int main()
+{
+    const double degrees = 45;
+
+    cv::Mat src = cv::imread("image.png", CV_LOAD_IMAGE_UNCHANGED);
+    cv::Mat dst;
+
+    cv::rotate2D(src, dst, degrees);
+
+    cv::imwrite("rotated.png", dst);
+
+    return 0;
+}
